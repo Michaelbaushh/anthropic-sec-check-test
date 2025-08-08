89314298eea045a31a0a97e86a776b992ef725c5
Michaelbaushh <62116262+Michaelbaushh@users.noreply.github.com>
2025-08-08 11:48:06 +0200

    Create insecure-app.js

---
+++ src/insecure-app.js
@@ -0,0 +1,18 @@
+// 🚨 Unsicherer Code mit SQL-Injection-Risiko
+
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+app.get('/user', function(req, res) {
+  const connection = mysql.createConnection({ /* ... */ });
+  const userId = req.query.id;
+  const query = `SELECT * FROM users WHERE id = '${userId}'`; // 🔥 unsicher!
+  
+  connection.query(query, function(error, results) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.listen(3000);
